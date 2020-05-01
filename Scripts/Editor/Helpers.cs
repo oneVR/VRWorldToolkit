2a57c36dcf04b5f4fc7a89a03722c6f5cec5fc5d
1 <4764355+SudoOne@users.noreply.github.com>
2020-05-01 15:55:14 +0300

    Improved post-process setup logic

--- Scripts/Editor/Helpers.cs
+++ Scripts/Editor/Helpers.cs
@@ -5,8 +5,6 @@ using VRC.SDKBase;
 using UnityEditor.PackageManager;
 using UnityEditor.PackageManager.Requests;
 using UnityEngine;
-using System.Reflection;
-using UnityEngine.Assertions;
 #if UNITY_POST_PROCESSING_STACK_V2
 using UnityEngine.Rendering.PostProcessing;
 using System.IO;
@@ -67,26 +65,41 @@ namespace VRCWorldToolkit
         {
             if (EditorUtility.DisplayDialog("Setup Post Processing?", "This will setup your scenes Reference Camera and make a new global volume using the included example Post Processing Profile", "OK", "Cancel"))
             {
-                if (Camera.main == null)
+                //Check if reference camera exists
+                if (!descriptor.ReferenceCamera)
                 {
-                    GameObject camera = new GameObject("Main Camera");
-                    camera.AddComponent<Camera>();
-                    camera.AddComponent<AudioListener>();
-                    camera.tag = "MainCamera";
+                    if (Camera.main == null)
+                    {
+                        GameObject camera = new GameObject("Main Camera");
+                        camera.AddComponent<Camera>();
+                        camera.AddComponent<AudioListener>();
+                        camera.tag = "MainCamera";
+                    }
+                    descriptor.ReferenceCamera = Camera.main.gameObject;
                 }
-                descriptor.ReferenceCamera = Camera.main.gameObject;
-                if (!Camera.main.gameObject.GetComponent<PostProcessLayer>())
-                    Camera.main.gameObject.AddComponent(typeof(PostProcessLayer));
-                PostProcessLayer postprocess_layer = Camera.main.gameObject.GetComponent(typeof(PostProcessLayer)) as PostProcessLayer;
+
+                //Make sure the post process layer exists and set it up
+                if (!descriptor.ReferenceCamera.gameObject.GetComponent<PostProcessLayer>())
+                    descriptor.ReferenceCamera.gameObject.AddComponent(typeof(PostProcessLayer));
+                PostProcessLayer postprocess_layer = descriptor.ReferenceCamera.gameObject.GetComponent(typeof(PostProcessLayer)) as PostProcessLayer;
                 postprocess_layer.volumeLayer = LayerMask.GetMask("Water");
-                PostProcessVolume volume = GameObject.Instantiate(PostProcessManager.instance.QuickVolume(16, 100f));
+
+                //Copy the example profile to the Post Processing folder
                 if (!Directory.Exists("Assets/Post Processing"))
                     AssetDatabase.CreateFolder("Assets", "Post Processing");
                 if (AssetDatabase.LoadAssetAtPath("Assets/Post Processing/SilentProfile.asset", typeof(PostProcessProfile)) == null)
                 {
-                    AssetDatabase.CopyAsset("Assets/VRWorldToolkit/Resources/PostProcessing/SilentProfile.asset", "Assets/Post Processing/SilentProfile.asset");
+                    string path = AssetDatabase.GUIDToAssetPath("eaac6f7291834264f97854154e89bf76");
+                    if (path != null)
+                    {
+                        AssetDatabase.CopyAsset(path, "Assets/Post Processing/SilentProfile.asset");
+                    }
                 }
-                volume.sharedProfile = (PostProcessProfile)AssetDatabase.LoadAssetAtPath("Assets/Post Processing/SilentProfile.asset", typeof(PostProcessProfile));
+
+                //Set up the post process volume
+                PostProcessVolume volume = GameObject.Instantiate(PostProcessManager.instance.QuickVolume(16, 100f));
+                if (File.Exists("Assets/Post Processing/SilentProfile.asset"))
+                    volume.sharedProfile = (PostProcessProfile)AssetDatabase.LoadAssetAtPath("Assets/Post Processing/SilentProfile.asset", typeof(PostProcessProfile));
                 volume.gameObject.name = "Post Processing Volume";
                 volume.gameObject.layer = LayerMask.NameToLayer("Water");
             }
