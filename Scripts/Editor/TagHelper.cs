ec120a5f4d4ad711a800ab722a4dd15370db08d2
1 <4764355+SudoOne@users.noreply.github.com>
2020-05-24 16:42:46 +0300

    Tag helper for checking if a tag exists

--- Scripts/Editor/TagHelper.cs
+++ Scripts/Editor/TagHelper.cs
@@ -26,5 +26,25 @@ namespace VRWorldToolkit
                 so.Update();
             }
         }
+
+        public static bool TagExists(string tag)
+        {
+            UnityEngine.Object[] asset = AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset");
+            if ((asset != null) && (asset.Length > 0))
+            {
+                SerializedObject so = new SerializedObject(asset[0]);
+                SerializedProperty tags = so.FindProperty("tags");
+
+                for (int i = 0; i < tags.arraySize; ++i)
+                {
+                    if (tags.GetArrayElementAtIndex(i).stringValue == tag)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
     }
 }
