51272c97d08a25a4e42069e78d15f75f0cfea23a
1 <4764355+oneVR@users.noreply.github.com>
2020-06-09 21:15:52 +0300

    Add namespace and summary

--- Scripts/Editor/ConsoleFlagUtil.cs
+++ Scripts/Editor/ConsoleFlagUtil.cs
@@ -5,33 +5,39 @@ using System.Reflection;
 using UnityEngine;
 using UnityEngine.Assertions;
 
-public static class ConsoleFlagUtil
+namespace VRWorldToolkit
 {
-    private static readonly System.Type systemType;
-    private static MethodInfo mMethod_GetConsoleErrorPause;
-    private static MethodInfo mMethod_SetConsoleErrorPause;
-
-    static ConsoleFlagUtil()
+    /// <summary>
+    /// Utility for setting and getting console error pause flag with reflection
+    /// </summary>
+    public static class ConsoleFlagUtil
     {
-        systemType = Assembly.Load("UnityEditor.dll").GetType("UnityEditor.ConsoleWindow");
-        Assert.IsNotNull(systemType);
-    }
+        private static readonly System.Type systemType;
+        private static MethodInfo mMethod_GetConsoleErrorPause;
+        private static MethodInfo mMethod_SetConsoleErrorPause;
 
-    public static bool GetConsoleErrorPause()
-    {
-        if (mMethod_GetConsoleErrorPause == null)
-            mMethod_GetConsoleErrorPause = systemType.GetMethod("GetConsoleErrorPause", BindingFlags.Static | BindingFlags.Public);
+        static ConsoleFlagUtil()
+        {
+            systemType = Assembly.Load("UnityEditor.dll").GetType("UnityEditor.ConsoleWindow");
+            Assert.IsNotNull(systemType);
+        }
 
-        Assert.IsNotNull(mMethod_GetConsoleErrorPause);
-        return (bool)mMethod_GetConsoleErrorPause.Invoke(null, null);
-    }
+        public static bool GetConsoleErrorPause()
+        {
+            if (mMethod_GetConsoleErrorPause == null)
+                mMethod_GetConsoleErrorPause = systemType.GetMethod("GetConsoleErrorPause", BindingFlags.Static | BindingFlags.Public);
 
-    public static void SetConsoleErrorPause(Boolean enabled)
-    {
-        if (mMethod_SetConsoleErrorPause == null)
-            mMethod_SetConsoleErrorPause = systemType.GetMethod("SetConsoleErrorPause", BindingFlags.Static | BindingFlags.Public);
+            Assert.IsNotNull(mMethod_GetConsoleErrorPause);
+            return (bool)mMethod_GetConsoleErrorPause.Invoke(null, null);
+        }
 
-        Assert.IsNotNull(mMethod_SetConsoleErrorPause);
-        mMethod_SetConsoleErrorPause.Invoke(null, new object[] { enabled });
+        public static void SetConsoleErrorPause(Boolean enabled)
+        {
+            if (mMethod_SetConsoleErrorPause == null)
+                mMethod_SetConsoleErrorPause = systemType.GetMethod("SetConsoleErrorPause", BindingFlags.Static | BindingFlags.Public);
+
+            Assert.IsNotNull(mMethod_SetConsoleErrorPause);
+            mMethod_SetConsoleErrorPause.Invoke(null, new object[] { enabled });
+        }
     }
 }
