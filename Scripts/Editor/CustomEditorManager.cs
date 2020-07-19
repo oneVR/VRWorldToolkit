b087cf74317c96ca76199658a3b0a33a4c7bfd1e
1 <4764355+oneVR@users.noreply.github.com>
2020-07-19 12:43:36 +0300

    Menu to disable custom editors

---
+++ Scripts/Editor/CustomEditorManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace VRWorldToolkit
+{
+    public class CustomEditorManager : MonoBehaviour
+    {
+        [MenuItem("VRWorld Toolkit/Custom Editors/Enable", false, -100)]
+        private static void EnableCustomEditors()
+        {
+            ScriptingDefineManager.RemoveScriptingDefine("VRWT_DISABLE_EDITORS");
+        }
+
+        [MenuItem("VRWorld Toolkit/Custom Editors/Enable", true)]
+        private static bool EnableCustomEditorsValidate()
+        {
+#if VRWT_DISABLE_EDITORS
+            return true;
+#else
+            return false;
+#endif
+        }
+
+        [MenuItem("VRWorld Toolkit/Custom Editors/Disable", false, -100)]
+        private static void DisableCustomEditors()
+        {
+            ScriptingDefineManager.AddScriptingDefine("VRWT_DISABLE_EDITORS");
+        }
+
+        [MenuItem("VRWorld Toolkit/Custom Editors/Disable", true)]
+        private static bool DisableCustomEditorsValidate()
+        {
+#if !VRWT_DISABLE_EDITORS
+            return true;
+#else
+            return false;
+#endif
+        }
+    }
+}
---
+++ Scripts/Editor/ScriptingDefineManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace VRWorldToolkit
+{
+    public class ScriptingDefineManager : MonoBehaviour
+    {
+
+        public static void AddScriptingDefine(string define)
+        {
+            var defines = PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup).Split(';').ToList();
+
+            if (defines.Contains(define)) return;
+
+            defines.Add(define);
+
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup, string.Join(";", defines));
+        }
+
+        public static void RemoveScriptingDefine(string define)
+        {
+            var defines = PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup).Split(';').ToList();
+
+            if (!defines.Contains(define)) return;
+
+            defines.Remove(define);
+
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup, string.Join(";", defines));
+        }
+
+        public static bool ScriptingDefineExists(string define)
+        {
+            var defines = PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup).Split(';');
+
+            return defines.Contains(define);
+        }
+    }
+}
