1d2feff056a3491cd550de5c5dce50a775d57d3b
1 <4764355+oneVR@users.noreply.github.com>
2020-12-27 12:32:13 +0200

    Validate function for Copy World ID MenuItem

--- Scripts/Editor/QuickFunctions.cs
+++ Scripts/Editor/QuickFunctions.cs
@@ -25,16 +25,25 @@ namespace VRWorldToolkit
 
                 if (pipelineManager)
                 {
-                    if (pipelineManager.blueprintId.Length > 0)
-                    {
-                        EditorGUIUtility.systemCopyBuffer = pipelineManager.blueprintId;
-                    }
-                    else
-                    {
-                        EditorUtility.DisplayDialog("No Blueprint ID", "The pipeline manager in the current scene has no Blueprint ID attached to it yet.", "Ok");
-                    }
+                    EditorGUIUtility.systemCopyBuffer = pipelineManager.blueprintId;
                 }
             }
+        }
+
+        [MenuItem("VRWorld Toolkit/Quick Functions/Copy World ID", true)]
+        private static bool CopyWorldIDValidate()
+        {
+            var descriptors = FindObjectsOfType(typeof(VRC_SceneDescriptor)) as VRC_SceneDescriptor[];
+
+            if (descriptors.Length is 1)
+            {
+                var pipelineManager = descriptors[0].GetComponent<PipelineManager>();
+
+                if (pipelineManager)
+                    return pipelineManager.blueprintId.Length > 0;
+            }
+
+            return false;
         }
     }
 }
