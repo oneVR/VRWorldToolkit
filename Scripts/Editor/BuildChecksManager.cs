0b8099b29cad7ade959b69fe0e752d23de3ec8f6
1 <4764355+oneVR@users.noreply.github.com>
2020-12-29 23:02:26 +0200

    Improve build checks manager to not do checks on avatar projects

--- Scripts/Editor/BuildChecksManager.cs
+++ Scripts/Editor/BuildChecksManager.cs
@@ -23,37 +23,54 @@ namespace VRWorldToolkit
 
             public bool OnBuildRequested(VRCSDKRequestedBuildType requestedBuildType)
             {
-                var pipelines = FindObjectsOfType(typeof(PipelineManager)) as PipelineManager[];
-
-                if (pipelines.Length > 1)
-                {
-                    var selection = EditorUtility.DisplayDialogComplex("VRWorld Toolkit: Multiple Pipeline managers!", "Multiple Pipeline Manager components found in scene.\r\n\r\nThis can break the upload process and cause you to not be able to load into the world.\r\n\r\nSelect Cancel Build if you want to fix the problem yourself or press Bypass to ignore the problem and continue.", "Fix And Continue", "Cancel Build", "Bypass");
-
-                    switch (selection)
-                    {
-                        case 0:
-                            WorldDebugger.RemoveBadPipelineManagers(pipelines).Invoke();
-                            break;
-                        case 1:
-                            return false;
-                    }
-                }
-
                 var descriptors = FindObjectsOfType(typeof(VRC_SceneDescriptor)) as VRC_SceneDescriptor[];
 
                 if (descriptors.Length > 0)
                 {
-                    var spawns = descriptors[0].spawns.Where(s => s != null).ToArray();
-                    var spawnsLength = descriptors[0].spawns.Length;
+                    var spawnProblems = false;
+                    var descriptor = descriptors[0];
 
-                    if (spawnsLength != spawns.Length)
+                    if (descriptor.spawns != null)
                     {
-                        var selection = EditorUtility.DisplayDialogComplex("VRWorld Toolkit: Null spawn points!", "Null spawn points set in Scene Descriptor.\r\n\r\nSpawning into a null spawn point will cause you get thrown back into your home world.\r\n\r\nSelect Cancel Build if you want to fix the problem yourself or press Bypass to ignore the problem and continue.", "Fix And Continue", "Cancel Build", "Bypass");
+                        var spawns = descriptor.spawns.Where(s => s != null).ToArray();
+                        var spawnsLength = descriptor.spawns.Length;
+
+                        if (spawnsLength != spawns.Length || spawnsLength == 0)
+                        {
+                            spawnProblems = true;
+                        }
+                    }
+                    else
+                    {
+                        spawnProblems = true;
+                    }
+
+                    if (spawnProblems)
+                    {
+                        var selection = EditorUtility.DisplayDialogComplex("VRWorld Toolkit: Problem spawn points!", "Null or empty spawn points set in Scene Descriptor.\r\n\r\nSpawning into a null or empty spawn point will cause you get thrown back into your home world.\r\n\r\nSelect Cancel Build if you want to fix the problem yourself or press Bypass to ignore the problem and continue.",
+                            "Fix And Continue", "Cancel Build", "Bypass");
 
                         switch (selection)
                         {
                             case 0:
-                                WorldDebugger.FixSpawns(descriptors[0]).Invoke();
+                                WorldDebugger.FixSpawns(descriptor).Invoke();
+                                break;
+                            case 1:
+                                return false;
+                        }
+                    }
+
+                    var pipelines = FindObjectsOfType(typeof(PipelineManager)) as PipelineManager[];
+
+                    if (pipelines.Length > 1)
+                    {
+                        var selection = EditorUtility.DisplayDialogComplex("VRWorld Toolkit: Multiple Pipeline managers!", "Multiple Pipeline Manager components found in scene.\r\n\r\nThis can break the upload process and cause you to not be able to load into the world.\r\n\r\nSelect Cancel Build if you want to fix the problem yourself or press Bypass to ignore the problem and continue.",
+                            "Fix And Continue", "Cancel Build", "Bypass");
+
+                        switch (selection)
+                        {
+                            case 0:
+                                WorldDebugger.RemoveBadPipelineManagers(pipelines).Invoke();
                                 break;
                             case 1:
                                 return false;
