269a666d86ec9ad4e0baeeb2811db5c4e70ed71c
BluWizard <17975114+BluWizard10@users.noreply.github.com>
2023-03-06 14:48:39 -0800

    Updated Layer Warnings for the VRC Mirror Script
    - Added a check against Mirrors if it's Reflect Layers are set to Everything, and will warn users in the Editor if so.
    - Added a check for the reserved2 Layer, which VRChat now uses for it's HUD, Main Menu, Tooltips and it's User Interface.
    - Modified the check for the UiMenu Layer as it still currently shows Nameplates.

--- Editor/CustomMirrorEditor.cs
+++ Editor/CustomMirrorEditor.cs
@@ -49,15 +49,21 @@ namespace VRWorldToolkit.Editor
 
                 if (mirrorMask.intValue == -1025)
                     EditorGUILayout.HelpBox("This mirror has default layers set. Unnecessary layers should be disabled to save on performance.", MessageType.Info);
+                    
+                if (mirrorMask.intValue == ~0)
+                    EditorGUILayout.HelpBox("This mirror has it's Reflect Layers set to Everything. This can lead to degraded performance in populated instances! Unnecessary Reflect Layers should be disabled to save on performance.", MessageType.Error);
 
                 if (Helper.LayerIncludedInMask(LayerMask.NameToLayer("UiMenu"), mirrorMask.intValue))
-                    EditorGUILayout.HelpBox("Having UiMenu enabled on mirrors causes VRChat UI elements to be rendered twice, causing a noticeable performance drop in populated instances.", MessageType.Warning);
+                    EditorGUILayout.HelpBox("Having UiMenu enabled on mirrors causes Nameplates to be rendered twice and should not be enabled on mirrors.", MessageType.Warning);
+                
+                if (Helper.LayerIncludedInMask(LayerMask.NameToLayer("reserved2"), mirrorMask.intValue))
+                    EditorGUILayout.HelpBox("Having reserved2 enabled on mirrors causes the VRChat HUD, Main Menu and Tooltips to be rendered twice, causing a noticeable performance drop in populated instances.", MessageType.Warning);
 
                 if (!Helper.LayerIncludedInMask(LayerMask.NameToLayer("MirrorReflection"), mirrorMask.intValue))
                     EditorGUILayout.HelpBox("Having the MirrorReflection layer disabled will stop the player from seeing themselves in the mirror.", MessageType.Warning);
 
                 if (Helper.LayerIncludedInMask(LayerMask.NameToLayer("PlayerLocal"), mirrorMask.intValue))
-                    EditorGUILayout.HelpBox("PlayerLocal is only meant to be seen in first-person view and should not be enabled on mirrors.", MessageType.Error);
+                    EditorGUILayout.HelpBox("PlayerLocal is only meant to be seen in first-person view and should not be enabled in mirrors.", MessageType.Error);
             }
 
             showExplanations = EditorGUILayout.Foldout(showExplanations, "VRChat Specific Layer Descriptions");
