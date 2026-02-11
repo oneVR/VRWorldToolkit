46e86846adf7705674a942397d6fe4e4abeb9909
BobyStar <bobystarosiris@gmail.com>
2026-02-11 03:11:54 -0600

    Added Support for UdonSharp
    This is a very weird implementation to be honest but it works well for not needing UdonSharp nor VRC.Udon referenced.
    The visual of the Sync Mode doesn't change until the user makes any action to cause an update to the editor.
    
    Known Issue: Adding an UdonSharpBehaviour to a GameObject that isn't selected does not apply the default.

--- Editor/DefaultUdonBehaviourSyncModeAssigner.cs
+++ Editor/DefaultUdonBehaviourSyncModeAssigner.cs
@@ -1,4 +1,7 @@
 #if VRC_SDK_VRCSDK3 && UDON
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 using UnityEditor;
 using UnityEngine;
 using VRC.SDK3.Components;
@@ -8,6 +11,8 @@ namespace VRWorldToolkit.Editor
 {
     public static class DefaultUdonBehaviourSyncModeAssigner
     {
+        private static bool waitingOnInspectorUpdate;
+        
         [InitializeOnLoadMethod]
         public static void Init()
         {
@@ -18,6 +23,51 @@ namespace VRWorldToolkit.Editor
         {
             if (component is AbstractUdonBehaviour udonBehaviour)
                 udonBehaviour.SyncMethod = Networking.SyncType.None;
+            // Wait a brief moment for any additional UdonBehaviours being added outside UnityEditor.ObjectFactory (like from UdonSharp).
+            else
+                CheckForPostAdditionalComponents(component, component.GetComponents<AbstractUdonBehaviour>());
+        }
+
+        private static async void CheckForPostAdditionalComponents(Component component, AbstractUdonBehaviour[] previousUdonBehaviours)
+        {
+            try
+            {
+                if (!component)
+                    return;
+
+                waitingOnInspectorUpdate = true;
+                EditorApplication.delayCall += DelayCalledInspector;
+                
+                while (waitingOnInspectorUpdate)
+                    await Task.Yield();
+            
+                AbstractUdonBehaviour[] newUdonBehaviours = component.GetComponents<AbstractUdonBehaviour>();
+            
+                if (newUdonBehaviours == null)
+                    return;
+
+                if (previousUdonBehaviours == null)
+                    previousUdonBehaviours = Array.Empty<AbstractUdonBehaviour>();
+                
+                // Check for newly added UdonBehaviours that didn't exist before.
+                foreach (AbstractUdonBehaviour udonBehaviour in newUdonBehaviours)
+                {
+                    if (previousUdonBehaviours.Contains(udonBehaviour))
+                        continue;
+                    
+                    if (udonBehaviour)
+                        udonBehaviour.SyncMethod = Networking.SyncType.None;
+                }
+            }
+            catch (Exception)
+            {
+                // Ignore
+            }
+        }
+        
+        private static void DelayCalledInspector()
+        {
+            waitingOnInspectorUpdate = false;
         }
     }
 }
